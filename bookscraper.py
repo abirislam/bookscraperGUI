a3efbce68b99b7a3bf0c9e00ac38d9da4ecc8315
Abir Islam <abir@abirs-mbp.myfiosgateway.com>
2022-11-26 22:38:30 -0500

    msg handling error fixed

--- bookscraper.py
+++ bookscraper.py
@@ -9,14 +9,18 @@ message = Label(root, text="If you have any issues or want to provide feedback, 
 message2 = Label(root, text="You can view other projects on my website at https://abirislam.github.io/").grid(row=1, column=0)
 spacer = Label(root, text=" ").grid(row=2, column=0)
 searchBoxLabel = Label(root, text="Enter Book Title: ").grid(row=3, column=0)
+searchMsgLabel = Label(root, text=" ")
+searchMsgLabel.grid(row=5, column=0)
 searchBox = Entry(root, width=45)
 searchBox.grid(row=4, column=0)
 
 #TODO work on search function
 def search():
+
+    #TODO error w msg handling
     searchMsg = "Searching for " + searchBox.get()
-    searchMsgLabel = Label(root, text=searchMsg)
-    searchMsgLabel.grid(row=3, column=0)
+    searchMsgLabel.config(text=searchMsg)
+    searchMsgLabel.grid(row=5, column=0)
 
 searchButton = Button(root, text="Search", padx=5, pady=5, command=search, fg="darkgreen").grid(row=4, column=1)
 
