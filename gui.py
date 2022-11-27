42f6fbbfa54092b1574805b9a2e9c087c05402b5
Abir Islam <abir@abirs-mbp.myfiosgateway.com>
2022-11-26 20:00:31 -0500

    commit 2: added gui elements

--- gui.py
+++ gui.py
@@ -2,8 +2,19 @@ from tkinter import *
 
 root = Tk()
 
-message = Label(root, text="Gui Testing 1 2 3")
 
-message.pack()
+# all elements on the GUI
+title = Label(root, text="Welcome to the Bookscraping GUI by Abir Islam").grid(row=0, column=0)
+message = Label(root, text="If you have any issues or want to provide feedback, please reach out").grid(row=1, column=0)
+message2 = Label(root, text="You can view other projects on my website at https://abirislam.github.io").grid(row=2, column=0)
+
+
+
+def search():
+    fooledya = Label(root, text="Sorry this feature is not implemented yet!")
+    fooledya.grid(row=4, column=0)
+
+#TODO work on search function
+searchButton = Button(root, text="Search", padx=5, pady=5, command=search).grid(row=3, column=1)
 
 root.mainloop()
