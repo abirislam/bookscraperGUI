426edec5ff540509101fe37bfb554647aca5dfed
Abir Islam <abir@abirs-mbp.myfiosgateway.com>
2022-11-26 20:08:39 -0500

    commit 2: entry fields

--- gui.py
+++ gui.py
@@ -7,14 +7,14 @@ root = Tk()
 title = Label(root, text="Welcome to the Bookscraping GUI by Abir Islam").grid(row=0, column=0)
 message = Label(root, text="If you have any issues or want to provide feedback, please reach out").grid(row=1, column=0)
 message2 = Label(root, text="You can view other projects on my website at https://abirislam.github.io").grid(row=2, column=0)
+searchBox = Entry(root).grid(row=3, column=0)
 
-
-
+#TODO work on search function
 def search():
     fooledya = Label(root, text="Sorry this feature is not implemented yet!")
     fooledya.grid(row=4, column=0)
 
-#TODO work on search function
-searchButton = Button(root, text="Search", padx=5, pady=5, command=search).grid(row=3, column=1)
+
+searchButton = Button(root, text="Search", padx=5, pady=5, command=search, fg="darkgreen").grid(row=3, column=1)
 
 root.mainloop()
