e3e4b6cf38e9ef5279832f152b17fc9ac864cf0a
Abir Islam <abir@abirs-mbp.myfiosgateway.com>
2022-12-01 16:05:30 -0500

    scraping works; links are not yet clickable

--- bookscraper.py
+++ bookscraper.py
@@ -1,35 +1,52 @@
 from tkinter import *
-from PIL import ImageTk, Image
+from bs4 import BeautifulSoup
+import requests
+import webbrowser
+from tkhtmlview import HTMLLabel
 
 root = Tk()
+
 root.title("Bookscraping GUI by Abir Islam")
 root.iconbitmap("bookscraperGUI/book_icon.png")
+
+# Framing
+frame = LabelFrame(root, text="Search here", padx=5, pady=5)
+frame.grid(row=3, column=0)
 
 # all elements on the GUI
 message = Label(root, text="If you have any issues or want to provide feedback, please reach out").grid(row=0, column=0)
 message2 = Label(root, text="You can view other projects on my website at https://abirislam.github.io/").grid(row=1, column=0)
 spacer = Label(root, text=" ").grid(row=2, column=0)
-searchBoxLabel = Label(root, text="Enter Book Title: ").grid(row=3, column=0)
-searchMsgLabel = Label(root, text=" ")
+searchBoxLabel = Label(frame, text="Enter Book Title: ").grid(row=3, column=0)
+searchMsgLabel = Label(frame, text=" ")
 searchMsgLabel.grid(row=5, column=0)
-searchBox = Entry(root, width=45)
+searchBox = Entry(frame, width=45)
 searchBox.grid(row=4, column=0)
-
-# Image stuff, replace of image of book cover later on! TODO
-booksImage = ImageTk.PhotoImage(Image.open("bookscraperGUI/books.png"))
-imageLabel = Label(root, image=booksImage)
-imageLabel.grid(row=6, column=0)
 
 #TODO work on search function
 def search():
-    searchMsg = "Searching for " + searchBox.get()
+    searchMsg = "Searching for " + searchBox.get() +"..."
     searchMsgLabel.config(text=searchMsg)
     searchMsgLabel.grid(row=5, column=0)
 
-def changeBookCover():
-    #this will change the book cover, we will add this into search.
-    return
+    string_concat = "https://libgen.is/search.php?req=" + searchBox.get() + "&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def"
+    html_text = requests.get(string_concat).text
 
-searchButton = Button(root, text="Search", padx=5, pady=5, command=search, fg="darkgreen").grid(row=4, column=1)
+    soup = BeautifulSoup(html_text, 'lxml')
+    links = soup.find_all('a', title="this mirror", limit=5)
+    counter = 1
+
+    for link in links:
+        link_href = link["href"]
+        herewego = str(counter) + ". " + link_href
+        result = Label(frame, text=herewego)
+        result.grid(row=5 + counter, column=0)
+        counter += 1
+
+    # print(links["href"])
+    # results.config(text=links["href"])
+
+
+searchButton = Button(frame, text="Search", padx=5, pady=5, command=search, fg="darkgreen").grid(row=4, column=1)
 
 root.mainloop()
